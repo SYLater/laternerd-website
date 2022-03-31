f77950d0ca903c28c77fecf25ef782fbf199f57a
SYLater <notjesseok@gmail.com>
2022-04-01 07:20:30 +1100

    added username&Email

--- website/auth.py
+++ website/auth.py
@@ -39,6 +39,7 @@ def sign_up():
     if request.method == 'POST':
         Email = request.form.get('Email')
         password = request.form.get('password')
+        username = request.form.get('username')
 
 
         user = User.query.filter_by(Email=Email).first()
@@ -46,10 +47,12 @@ def sign_up():
             flash('Email already exists.', category='error')
         elif len(Email) < 4:
             flash('Email must be greater than 3 characters.', category='error')
+        elif len(username) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
         elif len(password) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
-            new_user = User(Email=Email, password=generate_password_hash(password, method='sha256'))
+            new_user = User(username=username, Email=Email, password=generate_password_hash(password, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
