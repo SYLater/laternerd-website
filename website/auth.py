3734c1a0e57fc82e4dd6eec969bb8a3f9fdcba9d
SYLater <83808297+SYLater@users.noreply.github.com>
2022-04-01 09:22:10 +1100

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -52,7 +52,7 @@ def sign_up():
         elif len(password) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
-            new_user = User(username=username, Email=Email, password=generate_password_hash(password, method='sha256'))
+            new_user = User(Email=Email, username=username, password=generate_password_hash(password, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
